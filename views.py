a1b8d208d0a2b70ce298994d3cd218394c775069
KarelExiled <karel1998v@gmail.com>
2024-11-03 14:38:21 +0100

    Update views.py

--- views.py
+++ views.py
@@ -1,7 +1,1 @@
-import numpy as np
 
-def calculate_plot_properties(sensor_values, set_voltage):
-    """Utility to calculate any specific properties for plotting."""
-    normalized_voltage = set_voltage / 255 * 3.3
-    time = np.arange(len(sensor_values)) * 0.25  # Assuming 0.25 seconds interval
-    return time, normalized_voltage
